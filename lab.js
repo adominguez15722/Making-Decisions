b65fb1a164da90d83fd20f4f1a6cc18d9a510879
Anthony <adominguez15722@gmail.com>
2022-01-05 15:26:03 -0600

    Problem 2 is solved

--- lab.js
+++ lab.js
@@ -8,13 +8,13 @@
 
 //CODE HERE
 
-let lovesCode = false;
+// let lovesCode = true;
 
-if (lovesCode = true) {
-  console.log('I love to code!')
-} else {
-  console.log('Coding has its challenges.')
-}
+// if (lovesCode = true) {
+//   console.log('I love to code!')
+// } else {
+//   console.log('Coding has its challenges.')
+// }
 
 // For problems 2-3 use the following lines of code:
 var amysAge = 29
@@ -30,7 +30,18 @@ var brittanisBirthYear = 1986
 
 //CODE HERE
 
+if (amysAge > brittanisAge) {
+  console.log ('Amy is older')
+} 
 
+else if (amysAge < brittanisAge) {
+  console.log ('Brittani is older')
+}
+
+
+else {
+  console.log ('They are the same age')
+}
 ////////// PROBLEM 3 //////////
 
 /*
